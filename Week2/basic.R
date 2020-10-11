bd0cced4eade452b4a1a6988c6db93dd728a93a7
Shandy_10 <shandilya.s.v@outlook.com>
2020-10-11 22:28:23 +0530

    Initial commit

---
+++ Week2/basic.R
@@ -0,0 +1,21 @@
+#Basic Profile
+name = "Shandilya"
+degrees = 3
+Height <- 5.10
+ class(Height)
+ 
+ #calculating CTC
+ TA= 1000
+ NSA= 2000
+ BP= 15000
+ d=sum(TA,NSA,BP)
+ print(d)
+ loan = 6000
+ print(d- loan)
+ 
+ #Matrix
+ a=c(2,3,4,5)
+ b=c(6,7,8,9)
+ data=matrix(c(a,b),nrow = 4)
+ print(data)
+ 
